0c3428df2dd97c3dc9fc8e2735d0ae4e3c281074
aanginer <96923304+aanginer@users.noreply.github.com>
2022-03-06 12:24:02 -0700

    Create __init__.py

---
+++ scripts/utils/__init__.py
@@ -0,0 +1,24 @@
+import geometryDash as gd
+class client(object):
+    def __init__(self,name,id,location,module):
+        self.name = str(name) + '.client'
+        self.id = id
+        self.location = location
+        self.module = module
+    def __repr__(self):
+        return f"{self.name};{self.id}"
+
+    def client(self):
+        return self.name
+
+class exe(object):
+    def __init__(self,name,id):
+        self.name = name
+        self.id = id
+    
+    def __repr__(self):
+        return f"{self.name}.exe at {self.id}"
+
+    def run(self,func):
+        if self.id == 1722:
+            gd.run(func())
